50c3faf2146313ba7900b8449109dbadb5d4d1b1
Julia Matveeva <juamatveeva@gmail.com>
2018-11-26 01:14:02 +0300

    Initial data added to Perfomer table

---
+++ PerfomersServer/Migrations/20181125204607_initData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PerfomersServer.Migrations
+{
+    public partial class initData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Perfomer", true);
+            migrationBuilder.InsertData(
+                table: "Perfomer",
+                columns: new[] { "Id", "Name" },
+                values: new object[,] { { 1, "MISSIO" }, { 2, "IAMX" } ,
+                                        { 3, "Sir Sly" }, { 4, "The xx" },
+                                        { 5, "lovelytheband" }, { 6, "Two Door Cinema Club" } });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Perfomer", true);
+        }
+    }
+}

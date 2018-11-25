6f4d29378e8d108f734dc53581dfae72dbe7a385
Julia Matveeva <juamatveeva@gmail.com>
2018-11-26 01:22:06 +0300

    Venue model created

---
+++ VenuesService/Models/Venue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VenuesService.Models
+{
+    public class Venue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+
+
+
+
+    }
+}

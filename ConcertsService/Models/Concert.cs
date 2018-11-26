36d68d8662e4b1ccef091bae3c14464f34e7b6d2
Julia Matveeva <juamatveeva@gmail.com>
2018-11-26 04:06:43 +0300

    Concert model created

---
+++ ConcertsService/Models/Concert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConcertsService.Models
+{
+    public class Concert
+    {
+        public int Id { get; set; }
+
+        public int VenueId { get; set; }
+        public int PerfomerId { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}

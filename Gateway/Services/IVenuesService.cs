e54eb7e92731df28a76499b28338c97ea8a8d5b5
Julia Matveeva <juamatveeva@gmail.com>
2018-12-02 23:46:55 +0300

    venues service in gateway

---
+++ Gateway/Services/IVenuesService.cs
@@ -0,0 +1,15 @@
+﻿using Gateway.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gateway.Services
+{
+    public interface IVenuesService
+    {
+        Task<Venue> GetById(int id);
+        Task<bool> PostSchedule(Schedule schedule);
+        Task<bool> PutSchedule(Schedule schedule);
+    }
+}

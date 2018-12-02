2254208900f910497d4dd1d16e8a8dd7cdf4ec35
Julia Matveeva <juamatveeva@gmail.com>
2018-12-02 23:47:11 +0300

    perfomer service in gateway

---
+++ Gateway/Services/IPerfomersService.cs
@@ -0,0 +1,13 @@
+﻿using Gateway.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gateway.Services
+{
+    public interface IPerfomersService
+    {
+        Task<Perfomer> GetById(int id);
+    }
+}

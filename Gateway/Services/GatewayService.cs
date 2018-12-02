87557931a49ccf97ce900d178dfb9e8a611a4aab
Julia Matveeva <juamatveeva@gmail.com>
2018-12-02 23:47:41 +0300

    gateway as service

---
+++ Gateway/Services/GatewayService.cs
@@ -0,0 +1,59 @@
+﻿using Gateway.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gateway.Services
+{
+    public class GatewayService : IGatewayService
+    {
+        private readonly IConcertService concertService;
+        private readonly IPerfomersService perfomersService;
+        private readonly IVenuesService venuesService;
+
+        public GatewayService(IConcertService _concertService, 
+            IPerfomersService _perfomersService, IVenuesService _venuesService)
+        {
+            concertService = _concertService;
+            perfomersService = _perfomersService;
+            venuesService = _venuesService;
+        }
+
+        public async Task<List<Concert>> GetConcerts(int page, int size)
+        {
+            return await concertService.GetAll(page, size);
+        }
+
+        public async Task<Perfomer> GetPerfomerById(int id)
+        {
+            return await perfomersService.GetById(id);
+        }
+
+        public async Task<Venue> GetVenueById(int id)
+        {
+            return await venuesService.GetById(id);
+        }
+
+        public async Task<(bool, int)> PostConcert(Concert concert)
+        {
+            return await concertService.PostOne(concert);
+        }
+
+        public async Task<bool> PostSchedule(Schedule schedule)
+        {
+            return await venuesService.PostSchedule(schedule);
+        }
+
+        public async Task<bool> PutConcert(int id, Concert concert)
+        {
+            return await concertService.PutOne(id, concert);
+        }
+
+        public async Task<bool> PutSchedule(Schedule schedule)
+        {
+            return await venuesService.PutSchedule(schedule);
+        }
+
+    }
+}
---
+++ Gateway/Services/IGatewayService.cs
@@ -0,0 +1,19 @@
+﻿using Gateway.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gateway.Services
+{
+    public interface IGatewayService
+    {
+        Task<List<Concert>> GetConcerts(int page, int size);
+        Task<Perfomer> GetPerfomerById(int id);
+        Task<Venue> GetVenueById(int id);
+        Task<(bool, int)> PostConcert(Concert concert);
+        Task<bool> PostSchedule(Schedule schedule);
+        Task<bool> PutConcert(int id, Concert concert);
+        Task<bool> PutSchedule(Schedule schedule);
+    }
+}

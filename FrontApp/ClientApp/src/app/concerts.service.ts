0a414f5d7a355499b405bbc688f00b32dc6d5db1
Julia Matveeva <juamatveeva@gmail.com>
2018-12-12 04:30:15 +0300

    almost crud in concerts service

--- FrontApp/ClientApp/src/app/concerts.service.ts
+++ FrontApp/ClientApp/src/app/concerts.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Concert } from './concert';
 
 @Injectable()
 export class ConcertsService {
 
-  constructor() { }
+  private url = "https://localhost:44366/api/concerts";
 
+  constructor(private http: HttpClient) {
+  }
+
+  getConcerts() {
+    return this.http.get(this.url);
+  }
+
+  createConcert(concert: Concert) {
+    return this.http.post(this.url, concert);
+  }
+  updateConcert(concert: Concert) {
+
+    return this.http.put(this.url + '/' + concert.id, concert);
+  }
+  /*deleteConcert(id: number) {
+    return this.http.delete(this.url + '/' + id);
+  }*/
 }

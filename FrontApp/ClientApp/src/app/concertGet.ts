418c30d70fbf1455ab12f8b2bc58be4395cbe7ad
Julia Matveeva <juamatveeva@gmail.com>
2018-12-12 20:01:48 +0300

    Create concertGet.ts

---
+++ FrontApp/ClientApp/src/app/concertGet.ts
@@ -0,0 +1,10 @@
+
+export class ConcertGet
+{
+    constructor(
+      public id?: number,
+      public perfomerName?: string,
+      public venueName?: string,
+      public venueAdress?: string,
+      public date?: Date) { }
+  }

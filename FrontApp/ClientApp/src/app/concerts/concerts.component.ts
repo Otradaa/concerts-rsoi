f84edef3d9e6126c488890b3a710442b388bf44f
Julia Matveeva <juamatveeva@gmail.com>
2019-01-06 22:18:41 +0300

    created concerts component

---
+++ FrontApp/ClientApp/src/app/concerts/concerts.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Concert } from '../concert';
+import { Perfomer } from '../perfomer';
+import { Venue } from '../venue';
+import { ConcertGet } from '../concertGet';
+
+@Component({
+  selector: 'app-concerts',
+  templateUrl: './concerts.component.html',
+  styleUrls: ['./concerts.component.css']
+})
+export class ConcertsComponent implements OnInit {
+
+  concert: Concert = new Concert();   
+  perfomer: Perfomer = new Perfomer();
+  venue: Venue = new Venue();
+  concerts: ConcertGet[];              
+  venues: Venue[];
+  perfomers: Perfomer[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

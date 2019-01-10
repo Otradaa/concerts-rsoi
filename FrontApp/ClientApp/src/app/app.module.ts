2ca7adee00b4c98e51e3e3b4eaa3cca0e9623119
Julia Matveeva <juamatveeva@gmail.com>
2019-01-11 00:18:40 +0300

    added routing

--- FrontApp/ClientApp/src/app/app.module.ts
+++ FrontApp/ClientApp/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConcertsComponent } from './concerts/concerts.component';
 import { ConcertDetailComponent } from './concert-detail/concert-detail.component';
+import { ConcertCreateComponent } from './concert-create/concert-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConcertsComponent,
-    ConcertDetailComponent
+    ConcertDetailComponent,
+    ConcertCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/concerts', pathMatch: 'full' },
+      { path: 'concerts', component: ConcertsComponent },
+      { path: 'concerts/new', component: ConcertCreateComponent },
+      { path: 'concerts/:id', component: ConcertDetailComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

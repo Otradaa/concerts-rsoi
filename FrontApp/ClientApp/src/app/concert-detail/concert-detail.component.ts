b7a83254ea0ec39b57ee37453524cc0a90fd9d85
Julia Matveeva <juamatveeva@gmail.com>
2019-01-11 00:21:40 +0300

    component for put concert

--- FrontApp/ClientApp/src/app/concert-detail/concert-detail.component.ts
+++ FrontApp/ClientApp/src/app/concert-detail/concert-detail.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ConcertsService } from '../concerts.service';
+
+
 import { Concert } from '../concert';
 import { Perfomer } from '../perfomer';
 import { Venue } from '../venue';
 import { ConcertGet } from '../concertGet';
+import { ConcertFull } from '../concertFull';
 
 @Component({
   selector: 'app-concert-detail',
@@ -11,9 +15,32 @@ import { ConcertGet } from '../concertGet';
 })
 export class ConcertDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() concertInput: ConcertGet = new ConcertGet(1,"","","",new Date());
+  concert: Concert = new Concert(1);
+  fvenues: Venue[];
+  fperfomers: Perfomer[];
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.dataService.updateConcert(this.concert).subscribe((concertN: Concert) => this.concert = concertN);
+  }
+
+  constructor(private dataService: ConcertsService) {
+    this.concert.id = this.concertInput.id;
+    this.dataService.getPerfomers()
+      .subscribe((data: Perfomer[]) => this.fperfomers = data);
+    this.dataService.getVenues()
+      .subscribe((data: Venue[]) => this.fvenues = data);
+  }
 
   ngOnInit() {
+    
+
+    
   }
+
+  
+  
 
 }

fbede27623d33a53ddc3915b12689d322b806661
Julia Matveeva <juamatveeva@gmail.com>
2019-01-13 23:55:54 +0300

    added pagination

--- FrontApp/ClientApp/src/app/concerts/concerts.component.ts
+++ FrontApp/ClientApp/src/app/concerts/concerts.component.ts
@@ -5,6 +5,9 @@ import { Perfomer } from '../perfomer';
 import { Venue } from '../venue';
 import { ConcertGet } from '../concertGet';
 import { MessageService } from '../message.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ConcertsPage } from '../concertsPage';
+
 
 @Component({
   selector: 'app-concerts',
@@ -21,27 +24,58 @@ export class ConcertsComponent implements OnInit {
   venues: Venue[];
   perfomers: Perfomer[];
   changing = false;
+  concertP: ConcertsPage;
+
+  itemsPerPage: number;
+  totalItems: any;
+  page: any;
+  previousPage: any;
 
   constructor(private dataService: ConcertsService,
     public messageService: MessageService) { }
 
   ngOnInit() {
-    this.loadConcerts();    // загрузка данных при старте компонента
-
-  }
-  // получаем данные через сервис
-  loadConcerts() {
-    this.dataService.getConcerts()
-      .subscribe((data: ConcertGet[]) => this.concerts = data);
+       // загрузка данных при старте компонента
     this.dataService.getPerfomers()
       .subscribe((data: Perfomer[]) => this.perfomers = data);
     this.dataService.getVenues()
       .subscribe((data: Venue[]) => this.venues = data);
+    this.page = 1;
+    this.itemsPerPage = 10;
+    this.totalItems = 10;
+    this.loadPage(this.page);
   }
 
-  onSelect(selConcert: ConcertGet): void {
-    this.selectedConcert = selConcert;
+  loadPage(page: number) {
+    this.changing = false;
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.loadConcerts();
+    }
+  }
+
+  
+
+  // получаем данные через сервис
+  loadConcerts() {
+    this.dataService.getConcerts(this.page, this.itemsPerPage)
+      .subscribe((data: ConcertsPage) => {
+      this.concerts = data.concertsData,
+        this.totalItems = data.totalCount,
+        this.page = data.page,
+        this.itemsPerPage = data.pageSize});
     
   }
+  
+  onSelect(selConcert: ConcertGet) {
+    this.selectedConcert = selConcert;
+   
+  }
+  onPageChanged(value: any) {
+    this.selectedConcert = new ConcertGet(1, "", "", "", new Date());
+    this.changing = value;
+    this.loadConcerts();
+  }
 
+  
 }

b9b7d09a3921802b2e922a1b603f9959cff610b9
Julia Matveeva <juamatveeva@gmail.com>
2019-01-21 11:42:24 +0300

    Update Startup.cs

--- ConcertsService/Startup.cs
+++ ConcertsService/Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ConcertsService.Models;
 using ConcertsService.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using ConcertsService.Auth;
+using Microsoft.AspNetCore.Identity;
 
 namespace ConcertsService
 {
@@ -34,6 +37,29 @@ namespace ConcertsService
                     options.UseSqlServer(Configuration.GetConnectionString("ConcertsContext")));
 
             services.AddScoped<IConcertRepository, ConcertRepository>();
+
+            //services.AddIdentityCore<IdentityUser>(); 
+
+            var accountOptions = new TokenParameters()
+
+            {
+                ISSUER = "authServer",
+                AUDIENCE = "GateWay"
+            };
+            var tokenGenerator = new Token(accountOptions);
+
+            services.AddSingleton<IToken>(tokenGenerator);
+
+            services.AddSingleton<TokenParameters>(accountOptions);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = accountOptions.GetParameters();
+
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +75,7 @@ namespace ConcertsService
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
